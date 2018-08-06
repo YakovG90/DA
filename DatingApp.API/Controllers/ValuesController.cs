a69d7854f8c5486e190e616b8ff37715d6cab067
EMNICON-LOCAL\yakov.golovin <yakov.golovin@emnicon.de>
2018-08-06 16:09:22 +0200

    2newComponents

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.Api.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
