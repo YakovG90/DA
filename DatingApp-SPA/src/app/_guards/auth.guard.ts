2793a670b347cabed56bf85d152809cfbfd974f1
Yakov Golovin <yakov.golovin@emnicon.de>
2018-08-16 13:49:26 +0200

    End of section 9

--- DatingApp-SPA/src/app/_guards/auth.guard.ts
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -8,7 +8,7 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router, private alertify: AlertifyService) {}
-  canActivate():  boolean {
+  canActivate(): boolean {
     if (this.authService.loggedIn()) {
       return true;
     }
